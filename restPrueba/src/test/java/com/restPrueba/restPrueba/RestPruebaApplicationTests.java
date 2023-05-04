42ea5f6081db32784b5e896aed34f485f232b486
manuel104 <manuelorrego@outlook.com>
2023-05-03 21:49:27 -0500

    restPrueba

---
+++ restPrueba/src/test/java/com/restPrueba/restPrueba/RestPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restPrueba.restPrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
